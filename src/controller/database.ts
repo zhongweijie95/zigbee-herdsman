9b21b71ed144da4ecd712ae0537b3af1c1f1c5c5
zhongweijie <zhongweijie95@vip.qq.com>
2022-11-01 16:36:45 +0800

    提交

--- src/controller/database.ts
+++ src/controller/database.ts
@@ -2,6 +2,73 @@ import fs from 'fs';
 import {DatabaseEntry, EntityType} from './tstype';
 import sqlite3 from 'sqlite3';
 
+class SqliteDB {
+    private path: string;
+    private static db: sqlite3.Database;
+
+    private constructor(path: string, db: sqlite3.Database) {
+        this.path = path;
+        db = db;        
+    }
+
+    public static open(path: string): SqliteDB {
+        const db = new sqlite3.Database(path);
+        return new SqliteDB(path, db);
+    }
+
+    private static printErrorInfo(err: Error) {
+        console.log("Error Message:" + err.message);
+    }
+
+    public createTable (sql: string) {
+        SqliteDB.db.serialize(function(){
+            SqliteDB.db.run(sql, function(err: Error){
+                if(null != err){
+                    SqliteDB.printErrorInfo(err);
+                    return;
+                }
+            });
+        });
+    } 
+    
+    /// tilesData format; [[level, column, row, content], [level, column, row, content]]
+    public insertData(sql: string, objects: any){
+        SqliteDB.db.serialize(function(){
+            var stmt = SqliteDB.db.prepare(sql);
+            for(var i = 0; i < objects.length; ++i){
+                stmt.run(objects[i]);
+            }
+        
+            stmt.finalize();
+        });
+    };
+
+    public queryData(sql: string, callback: (rows: any) => void){
+        SqliteDB.db.all(sql, function(err, rows){
+            if(null != err){
+                SqliteDB.printErrorInfo(err);
+                return;
+            }
+     
+            /// deal query data.
+            if(callback){
+                callback(rows);
+            }
+        });
+    }
+
+    public executeSql(sql: string){
+        SqliteDB.db.run(sql, function(err: Error){
+            if(null != err){
+                SqliteDB.printErrorInfo(err);
+            }
+       });
+    }
+
+    public close() {
+        SqliteDB.db.close();
+    }      
+}
 
 class Database {
     private entries: {[id: number]: DatabaseEntry};
@@ -16,35 +83,21 @@ class Database {
         const entries: {[id: number]: DatabaseEntry} = {};
 
         if (fs.existsSync(path)) {
-            const db = new sqlite3.Database(path);
-
-            db.serialize(() => {
-                db.get("SELECT json FROM config where id == 1", (err, row) => {
-                    //console.log(row.id + ": " + row.info);
-                    const rows = (row.json as string).split('\n').map((r) => r.trim()).filter((r) => r != '');
-                    for (const row of rows) {
-                        const json = JSON.parse(row);
-                        if (json.hasOwnProperty('id')) {
-                            entries[json.id] = json;
-                        }
-                    }                                            
-                });            
+            const db = SqliteDB.open(path);
+            db.queryData("SELECT json FROM config where id == 1", (row) => {
+                const rows = (row.json as string).split('\n').map((r) => r.trim()).filter((r) => r != '');
+                for (const row of rows) {
+                    const json = JSON.parse(row);
+                    if (json.hasOwnProperty('id')) {
+                        entries[json.id] = json;
+                    }
+                }
             });
-
             db.close();
-        }else{
-            const db = new sqlite3.Database(path);
-
-            db.serialize(() => {
-                db.run("CREATE TABLE config (id integer, json TEXT)");
-
-                /*const stmt = db.prepare("INSERT INTO json VALUES (?)");
-                stmt.run("{}");
-                stmt.finalize();*/
-                
-                db.run("INSERT INTO config VALUES (1, '{}')");
-            });
-
+        }else{ 
+            const db = SqliteDB.open(path);
+            db.createTable("CREATE TABLE config (id integer, json TEXT)");
+            db.insertData("INSERT INTO config VALUES (?, ?)", [[1, '{}']]);
             db.close();            
         }
 
@@ -98,24 +151,15 @@ class Database {
     }
 
     public write(): void {
-        /*const tmpPath = this.path + '.tmp';
-        fs.writeFileSync(tmpPath, lines.join('\n'));
-        fs.renameSync(tmpPath, this.path);*/
+        const lines = [];
+        for (const DatabaseEntry of Object.values(this.entries)) {
+            const json = JSON.stringify(DatabaseEntry);
+            lines.push(json);
+        }
 
-        const db = new sqlite3.Database(this.path);
-
-        db.serialize(() => {
-            const lines = [];
-            for (const DatabaseEntry of Object.values(this.entries)) {
-                const json = JSON.stringify(DatabaseEntry);
-                lines.push(json);
-            }
-
-            db.run("UPDATE config SET json = ? where id == 1", lines.join('\n'));
-        });
-
-        db.close();
-
+        const db = SqliteDB.open(this.path);
+        db.executeSql("UPDATE config SET json = " + lines.join('\n') + " where id == 1");
+        db.close();         
     }
 }
 
