823e116398b8df5241158b93a1463c84180fc513
zhongweijie <zhongweijie95@vip.qq.com>
2022-11-01 09:41:26 +0800

    提交

--- src/controller/database.ts
+++ src/controller/database.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import {DatabaseEntry, EntityType} from './tstype';
+import sqlite3 from 'sqlite3';
+
 
 class Database {
     private entries: {[id: number]: DatabaseEntry};
@@ -14,13 +16,36 @@ class Database {
         const entries: {[id: number]: DatabaseEntry} = {};
 
         if (fs.existsSync(path)) {
-            const rows = fs.readFileSync(path, 'utf-8').split('\n').map((r) => r.trim()).filter((r) => r != '');
-            for (const row of rows) {
-                const json = JSON.parse(row);
-                if (json.hasOwnProperty('id')) {
-                    entries[json.id] = json;
-                }
-            }
+            const db = new sqlite3.Database(path);
+
+            db.serialize(() => {
+                db.get("SELECT json FROM config where id == 1", (err, row) => {
+                    //console.log(row.id + ": " + row.info);
+                    const rows = (row.json as string).split('\n').map((r) => r.trim()).filter((r) => r != '');
+                    for (const row of rows) {
+                        const json = JSON.parse(row);
+                        if (json.hasOwnProperty('id')) {
+                            entries[json.id] = json;
+                        }
+                    }                                            
+                });            
+            });
+
+            db.close();
+        }else{
+            const db = new sqlite3.Database(path);
+
+            db.serialize(() => {
+                db.run("CREATE TABLE config (id integer, json TEXT)");
+
+                /*const stmt = db.prepare("INSERT INTO json VALUES (?)");
+                stmt.run("{}");
+                stmt.finalize();*/
+                
+                db.run("INSERT INTO config VALUES (1, '{}')");
+            });
+
+            db.close();            
         }
 
         return new Database(entries, path);
@@ -73,14 +98,24 @@ class Database {
     }
 
     public write(): void {
-        const lines = [];
-        for (const DatabaseEntry of Object.values(this.entries)) {
-            const json = JSON.stringify(DatabaseEntry);
-            lines.push(json);
-        }
-        const tmpPath = this.path + '.tmp';
+        /*const tmpPath = this.path + '.tmp';
         fs.writeFileSync(tmpPath, lines.join('\n'));
-        fs.renameSync(tmpPath, this.path);
+        fs.renameSync(tmpPath, this.path);*/
+
+        const db = new sqlite3.Database(this.path);
+
+        db.serialize(() => {
+            const lines = [];
+            for (const DatabaseEntry of Object.values(this.entries)) {
+                const json = JSON.stringify(DatabaseEntry);
+                lines.push(json);
+            }
+
+            db.run("UPDATE config SET json = ? where id == 1", lines.join('\n'));
+        });
+
+        db.close();
+
     }
 }
 
